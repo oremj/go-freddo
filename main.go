42f9c2f2356102bffdc617b6d64586ca14d727aa
Jeremiah Orem <oremj@oremj.com>
2014-02-03 15:15:36 -0800

    Add basic auth.

--- main.go
+++ main.go
@@ -6,11 +6,15 @@ import (
 	"net/http"
 
 	"github.com/codegangsta/martini"
+	"github.com/codegangsta/martini-contrib/auth"
+
 	"github.com/oremj/go-freddo/freddo"
 )
 
 var addr = flag.String("addr", ":8080", "Bind address.")
-var config = flag.String("config", "", "config location")
+var config = flag.String("config", "", "Config location.")
+var authUser = flag.String("authuser", "updater", "Basic auth user.")
+var authPass = flag.String("authpass", "updater", "Basic auth password.")
 
 func main() {
 	flag.Parse()
@@ -25,6 +29,6 @@ func main() {
 	}
 
 	m := martini.Classic()
-	m.Post(`/update/(?P<app_name>\w+?)/`, freddo.UpdateApp)
+	m.Post(`/update/(?P<app_name>\w+?)/`, auth.Basic(*authUser, *authPass), freddo.UpdateApp)
 	log.Fatal(http.ListenAndServe(*addr, m))
 }
